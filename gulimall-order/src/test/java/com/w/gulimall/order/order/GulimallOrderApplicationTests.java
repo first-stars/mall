7c2c167c663d7131f2bbdd1e34deea7711b098c2
xin <973169778@qq.com>
2022-12-27 15:19:55 +0800

    项目初始结构创建

---
+++ gulimall-order/src/test/java/com/w/gulimall/order/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w.gulimall.order.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-ware/src/test/java/com/w/gulimall/ware/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w.gulimall.ware.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
